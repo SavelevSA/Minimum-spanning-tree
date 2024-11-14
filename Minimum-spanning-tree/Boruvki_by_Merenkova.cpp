937dd15e7069df55f3349c7682603c4add1818b0
er-se <hunnerseh@mail.ru>
2024-11-14 20:55:16 +0300

    борувки
    готовые борувки!

--- Minimum-spanning-tree/Boruvki_by_Merenkova.cpp
+++ Minimum-spanning-tree/Boruvki_by_Merenkova.cpp
@@ -5,29 +5,12 @@
 #include "Algorithms.h"
 
 
+// Структура для представления рёбер графа
 struct Edge {
     int src, dest, weight;
 };
 
-struct Graph {
-    int V, E;
-    std::vector<Edge> edges;
-
-    Graph(int V, int E) {
-        this->V = V;
-        this->E = E;
-        edges.resize(E);
-    }
-
-    // функция для добавления рёбра
-    void addEdge(int i, int src, int dest, int weight) {
-        edges[i].src = src;
-        edges[i].dest = dest;
-        edges[i].weight = weight;
-    }
-};
-
-// Функция поиска (find) для Union-Find структуры
+// Функция для поиска (find) в Union-Find структуре
 int find(std::vector<int>& parent, int i) {
     if (parent[i] != i)
         parent[i] = find(parent, parent[i]);
@@ -49,10 +32,21 @@ void Union(std::vector<int>& parent, std::vector<int>& rank, int x, int y) {
     }
 }
 
-// Алгоритм Борувки для поиска мин. ост дерева (mst)
-void boruvkaMST(Graph& graph) {
-    int V = graph.V;
-    int E = graph.E;
+// Алгоритм Борувки для поиска минимального остовного дерева (МОД), принимающий матрицу смежности
+int Boruvki_by_Merenkova(std::vector<std::vector<int>>& matrix) {
+    int V = matrix.size();  // Количество вершин
+    std::vector<Edge> edges;  // Список рёбер
+
+    // Преобразуем матрицу смежности в список рёбер
+    for (int i = 0; i < V; ++i) {
+        for (int j = i + 1; j < V; ++j) {
+            if (matrix[i][j] != 0 && matrix[i][j] != INT_MAX) {  // INT_MAX используется для отсутствующих рёбер
+                edges.push_back({i, j, matrix[i][j]});
+            }
+        }
+    }
+
+    int E = edges.size();
     std::vector<int> parent(V);
     std::vector<int> rank(V, 0);
 
@@ -63,20 +57,21 @@ void boruvkaMST(Graph& graph) {
     int numTrees = V;
     int MSTweight = 0;
 
+    // Основной цикл алгоритма Борувки
     while (numTrees > 1) {
         // Инициализация минимальных рёбер для каждого компонента
         std::vector<Edge> cheapest(V, { -1, -1, INT_MAX });
 
         // Находим минимальное ребро для каждого компонента
         for (int i = 0; i < E; ++i) {
-            int set1 = find(parent, graph.edges[i].src);
-            int set2 = find(parent, graph.edges[i].dest);
+            int set1 = find(parent, edges[i].src);
+            int set2 = find(parent, edges[i].dest);
 
             if (set1 != set2) {
-                if (graph.edges[i].weight < cheapest[set1].weight)
-                    cheapest[set1] = graph.edges[i];
-                if (graph.edges[i].weight < cheapest[set2].weight)
-                    cheapest[set2] = graph.edges[i];
+                if (edges[i].weight < cheapest[set1].weight)
+                    cheapest[set1] = edges[i];
+                if (edges[i].weight < cheapest[set2].weight)
+                    cheapest[set2] = edges[i];
             }
         }
 
@@ -88,8 +83,6 @@ void boruvkaMST(Graph& graph) {
 
                 if (set1 != set2) {
                     MSTweight += cheapest[i].weight;
-                    std::cout << "Ребро " << cheapest[i].src << " - " << cheapest[i].dest
-                              << " включено в МОД с весом " << cheapest[i].weight << std::endl;
                     Union(parent, rank, set1, set2);
                     numTrees--;
                 }
@@ -97,5 +90,5 @@ void boruvkaMST(Graph& graph) {
         }
     }
 
-    std::cout << "Вес МОД: " << MSTweight << std::endl;
+    return MSTweight;
 }
